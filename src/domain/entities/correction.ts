39d0afe98321582f6bc8f237c5a8e5c1432ec260
jguibrandao <jguibrandao@gmail.com>
2022-03-01 02:20:20 -0300

    Setting class Correction who sets the correction of the student project.

--- src/domain/entities/correction.ts
+++ src/domain/entities/correction.ts
@@ -1,3 +1,19 @@
-class Correction {
-    
+import { Entity } from "../../core/domain/entity/Entity";
+
+type CorrectionProps = {
+    grade: number;
+    submissionId: string;
+    createdAt: Date;
+}
+
+export class Correction extends Entity<CorrectionProps> {
+    private constructor(props: CorrectionProps, id?: string) {
+        super(props, id);
+    }
+
+    public create(props: CorrectionProps, id?: string) {
+        const correction = new Correction(props, id);
+
+        return correction;
+    }
 }
