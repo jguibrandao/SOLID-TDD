21d2bbf8209e635d01c5a92f9db6e2516a3c1797
jguibrandao <jguibrandao@gmail.com>
2022-03-02 15:35:18 -0300

    returning id so we can test it

--- src/core/domain/entity/Entity.ts
+++ src/core/domain/entity/Entity.ts
@@ -4,6 +4,8 @@ export abstract class Entity<T> {
     protected _id: string;
     public props: T;
 
+    get id() { return this._id; } // returning id for tests
+
     constructor(props: T, id?:string) {
         this.props = props;
         this._id = id ?? `${Math.random()}`;
