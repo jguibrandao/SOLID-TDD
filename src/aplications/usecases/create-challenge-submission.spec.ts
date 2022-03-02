741778ab03dd367beb7ac1b920f3c13e6bc81a3c
jguibrandao <jguibrandao@gmail.com>
2022-03-02 15:35:02 -0300

    adaptating test to a appart local test

--- src/aplications/usecases/create-challenge-submission.spec.ts
+++ src/aplications/usecases/create-challenge-submission.spec.ts
@@ -1,12 +1,35 @@
+import { Challenge } from "../../domain/entities/challenge";
+import { Student } from "../../domain/entities/student";
+import { inMemoryChallengesRepository } from "../../tests/repositories/in-memory-challenges-repository";
+import { inMemoryStudentsRepository } from "../../tests/repositories/in-memory-students-repository";
 import { CreateChallengeSubmission } from "./create-challenge-submission";
 
 describe('Create challenge submission use case', () => {
     it('should be able to create a new challenge submission', async () => {
-        const sut = new CreateChallengeSubmission();
+        const studentsRepository = new inMemoryStudentsRepository();
+        const challengesRepository = new inMemoryChallengesRepository();
+
+        const student = Student.create({
+            name: 'João',
+            email: 'example@example.com'
+        })
+
+        const challenge = Challenge.create({
+            title: 'Test challenge',
+            instructionsUrl: 'www.example.com'
+        })
+
+        studentsRepository.items.push(student)
+        challengesRepository.items.push(challenge)
+
+        const sut = new CreateChallengeSubmission(
+            studentsRepository,
+            challengesRepository
+        );
 
         const response = await sut.execute({
-            studentId: 'test-student-id',
-            challengeId: 'test-challenge-id'
+            studentId: student.id,
+            challengeId: challenge.id
         });
 
         expect(response).toBeTruthy()
