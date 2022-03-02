e0250ff7d8cc1a5903adf9ac4189e6c6e27e2703
jguibrandao <jguibrandao@gmail.com>
2022-03-02 01:37:53 -0300

    creating validations for our data

--- src/aplications/usecases/create-challenge-submission.ts
+++ src/aplications/usecases/create-challenge-submission.ts
@@ -1,4 +1,6 @@
 import { Submission } from "../../domain/entities/submission"
+import { ChallengesRepository } from "../repositories/ChallengesRepository"
+import { StudentsRepository } from "../repositories/StudentsRepository"
 
 type CreateChallengeSubmissionRequest = {
     studentId: string;
@@ -6,7 +8,24 @@ type CreateChallengeSubmissionRequest = {
 }
 
 export class CreateChallengeSubmission {
-    execute({ studentId, challengeId }: CreateChallengeSubmissionRequest) {
+    constructor(
+        private stundentsRepository: StudentsRepository,
+        private challengesRepository: ChallengesRepository,
+    ) {}
+
+    async execute({ studentId, challengeId }: CreateChallengeSubmissionRequest) {
+       const student = await this.challengesRepository.findById(challengeId)
+
+        if (!student) {
+            throw new Error('Student does not exists')
+        }
+
+        const challenge = await this.stundentsRepository.findById(studentId)
+
+        if (!challenge) {
+            throw new Error('Challenge does not exists')
+        }
+
         const submission = Submission.create({
             studentId,
             challengeId,
